33d57c9a210a72fa65855be77b1eb990f587b8c1
aceiny <yzeraibi2000@gmail.com>
2025-02-17 22:44:30 +0100

    fix: update Redis configuration to ensure password is set and remove commented-out options

--- server/config/redis-config.ts
+++ server/config/redis-config.ts
@@ -1,13 +1,8 @@
 import { getEnvOrFatal } from 'common/utils/env.util';
 import { RedisOptions } from 'ioredis';
-import { ConnectionOptions } from 'tls';
 
 export const RedisConfig: RedisOptions = {
   host: getEnvOrFatal('REDIS_HOST'),
   port: getEnvOrFatal('REDIS_PORT'),
-  /*username: getEnvVar("REDIS_USERNAME"),
-  password: getEnvVar("REDIS_PASSWORD"),
-  tls: {
-    servername: getEnvVar("REDIS_SERVER_NAME"),
-  },*/ // local development
+  password: getEnvOrFatal("REDIS_PASSWORD"),
 };
