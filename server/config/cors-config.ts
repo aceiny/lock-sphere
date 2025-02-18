574db77fd8d85344b7a8ca1bdf6717a004883f0b
aceiny <yzeraibi2000@gmail.com>
2025-02-18 02:03:38 +0100

    fix: update CORS configuration to allow specific origins and enhance security headers

--- server/config/cors-config.ts
+++ server/config/cors-config.ts
@@ -1,4 +1,13 @@
-export const CorsConfig = {
-  origin: true,
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
+import { getEnvOrFatal } from "common/utils/env.util";
+
+export const CorsConfig : CorsOptions= {
+  origin: [
+    getEnvOrFatal('FRONTEND_URL'),
+    'http://localhost:3001'
+
+  ],
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 };
